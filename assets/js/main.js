1b40d3d4bdc6d05859ee5e397e245de62c10da67
mukund-9652 <mukundravisankaran2001@gmail.com>
2026-07-18 19:54:36 +0530

    latest image

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,18 @@
     const current = html.getAttribute('data-theme');
     const next = current === 'dark' ? 'light' : 'dark';
     html.setAttribute('data-theme', next);
+    
+    // Swap Hero Image
+    const heroImage = document.getElementById('heroImage');
+    if (heroImage) {
+      heroImage.src = next === 'dark' ? 'assets/img/mukund-dark-coat.png' : 'assets/img/mukund-light-coat.png';
+    }
+
+    // Swap About Image
+    const aboutImage = document.getElementById('aboutImage');
+    if (aboutImage) {
+      aboutImage.src = next === 'dark' ? 'assets/img/mukund-dark-coat-about.png' : 'assets/img/mukund-light-coat-about.png';
+    }
   });
 
   // ----------------------------------------------------------------
